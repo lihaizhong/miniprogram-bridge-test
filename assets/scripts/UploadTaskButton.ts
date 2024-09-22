4b4915e9a2bc69dd0121ab177faf744515a41823
sky <854323752@qq.com>
2024-09-22 18:22:06 +0800

    fix: 移除nanoid

--- assets/scripts/UploadTaskButton.ts
+++ assets/scripts/UploadTaskButton.ts
@@ -1,15 +1,30 @@
 import { _decorator, Component } from 'cc';
 import Inge, { configure, log } from 'inge-bridge';
 import { format } from 'date-fns';
-import { nanoid } from 'nanoid';
 
 const { ccclass, property } = _decorator;
+
+/**
+ * 生成UUID
+ * @returns {string}
+ */
+export function generateUUID(): string {
+    let d = Date.now()
+  
+    return 'xxxx-xxxx-4xxx-yxxx-xxxx'.replace(/[xy]/g, (c) => {
+      // eslint-disable-next-line no-bitwise
+      const r = (d + Math.random() * 16) % 16 | 0
+      d = Math.floor(d / 16)
+      // eslint-disable-next-line no-bitwise, no-mixed-operators
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+    })
+  }
 
 @ccclass('UploadTaskButton')
 export class UploadTaskButton extends Component {
     onUploadTask(event) {
         Inge.services.uploadTaskCompletionBehavior({
-            cpOutBizNo: nanoid(),
+            cpOutBizNo: generateUUID(),
             gameRoleId: 'test_role_01',
             buyerId: configure.getUserId(),
             actionFinishDate: format(new Date(), 'yyyy-MM-dd hh:mm:ss')
